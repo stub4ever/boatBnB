c190c188b285ca6382f2b0dc065ffc2aa9f5de33
Matthijs Kolkman <matthijs.h.j@Live.nl>
2016-03-08 17:58:47 +0100

    Added routes

---
+++ app/controllers/boats_controller.rb
@@ -0,0 +1,6 @@
+class BoatsController < ApplicationController
+  def index
+    # @boats =
+    # TODO: retrieve all the boats of the app
+  end
+end
---
+++ app/controllers/owner/boats_controller.rb
@@ -0,0 +1,8 @@
+module Owner
+  class BoatsController < ApplicationController
+    def index
+      # @boats =
+      # TODO: retrieve all the boats of the current user
+    end
+  end
+end
