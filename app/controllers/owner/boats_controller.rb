d334d6ac808d26ebe446041ac20e9d72825c182a
Melchior Rutte <melchior.rutte@gmail.com>
2016-03-09 16:10:11 +0100

    add controllers to the app

---
+++ app/controllers/owner/boats_controller.rb
@@ -0,0 +1,49 @@
+class Owner::BoatsController < ApplicationController
+  before_action :find_boat, only: [:show, :edit, :update, :destroy]
+  def index
+    @boats = Boat.all
+  end
+
+  def show
+    @full_name = @boat.owner.first_name + " " + @boat.owner.last_name
+  end
+
+  def new
+    @boat = Boat.new
+    @owner = current_user
+  end
+
+  def create
+    @boat = Boat.new(boat_param)
+    @owner = current_user
+    @boat.owner = @owner
+    @boat.save!
+    redirect_to owner_boat_path(@boat)
+  end
+
+  def edit
+  end
+
+  def update
+    @boat.update!(boat_param)
+    redirect_to owner_boat_path(@boat)
+  end
+
+  def destroy
+    @boat.destroy!
+    redirect_to profile_path(@boat)
+  end
+
+  private
+
+  def boat_param
+    params.require(:boat).permit(:name, :description, :category, :size, :city, :address, :price, :photo, :photo_cache)
+
+  end
+
+  def find_boat
+   @boat = Boat.find(params[:id])
+ end
+
+
+end
