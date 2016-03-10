fada2a34c14d64c582e27b6a93b2282fb37cd69f
Matthijs Kolkman <matthijs.h.j@Live.nl>
2016-03-10 12:24:17 +0100

    add new route for registration and update controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
+
 
   resources :profile, only: [:show]
 
